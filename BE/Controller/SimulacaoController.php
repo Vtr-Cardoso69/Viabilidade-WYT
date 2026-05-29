d4c40d3e6fe47f25c29b1f7412799b78393b80f2
WPOTC <gutomergr.s@gmail.com>
2026-05-29 07:21:19 -0300

    excluí o restante obsoleto

--- BE/Controller/SimulacaoController.php
+++ BE/Controller/SimulacaoController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../Model/SimulacaoModel.php';
+require_once '../Model/SimulacaoModel.php';
 
 class SimulacaoController {
 
@@ -10,27 +10,9 @@ class SimulacaoController {
         $this->model = new SimulacaoModel($pdo);
     }
 
-    public function listarCidades() {
-        return $this->model->listarCidades();
-    }
-
-    public function listarEmpresas() {
-        return $this->model->listarEmpresas();
-    }
-
-    public function calcularProbabilidadeSucesso($cidade_id, $empresa_id, $investimento = null, $quant_ancoras = null, $preco_medio = null) {
-        if (func_num_args() == 3) {
-            return $this->model->calcularProbabilidadeSucesso($cidade_id, $empresa_id, $investimento);
-        }
-
-        return $this->model->simularViabilidade(
-            $cidade_id,
-            $empresa_id,
-            $investimento,
-            $quant_ancoras,
-            $preco_medio
-        );
-    }
+    
 }
+
+
 
 ?>
