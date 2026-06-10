9f971d2499fde0658d1c7084d3e5aa17ec514101
Ana Luisa <analuisaanselmo2008@gmail.com>
2026-06-10 13:43:03 -0300

    fazendo css do index

--- index.php
+++ index.php
@@ -8,12 +8,15 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INICIO</title>
-    <link rel="stylesheet" href="CSS/.css">
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
 <body>
+
+    <nav class="nav1">
+
    <header>
-        <img width="100" height="100" src="img/bussola.png" alt="Bússola">
-        <img width="100" height="100" src="img/logo.png" alt="Logo">
+        <img width="100" height="100" src="img/bussola.png" alt="Bússola" class="bussola" id="bussola">
+        <img width="100" height="100" src="img/logo.png" alt="Logo" class="logo">
         <?php
     if (isset($_SESSION['empresa_id'])) {
         echo "<p><a href='Pages/perfilUsuarios.php?id=" . $_SESSION['empresa_id'] . "'>Bem-vindo(a), " . $_SESSION['nome'] . "!</a></p>";
@@ -23,14 +26,33 @@ session_start();
     ?>
     </header>
     
-    <nav>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
-             <a href="#"></a>
-            <a href="#"></a>
+   
+          
     </nav>
+    
+    <nav class="nav2" id="menu">
+
+     <div>
+      <h3>SUPORTE</h3>
+      <ul>
+        <li><a href="#">Central de Ajuda</a></li>
+        <li><a href="#">Política de Privacidade</a></li>
+        <li><a href="#">Termos de Uso</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+    </div>
+    
+    <div>
+      <h3>SOCIAL</h3>
+      <ul>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Tiktok</a></li>
+      </ul>
+    </div>
+
+
+   </nav>
 
     <?php
 if (isset($_SESSION['empresa_id'])) {
@@ -44,36 +66,46 @@ if (isset($_SESSION['empresa_id'])) {
 }
 ?>
 
-    <section>
+   <div class="conteudo">
+
+    <section class="primeiro">
             <h1>Bem-vindo ao WYT!</h1>
             <p>Na WYT, transformamos ideias em oportunidades por meio de análises estratégicas e estudos de viabilidade. Oferecemos soluções precisas e seguras para decisões inteligentes e sustentáveis.</p>
     </section>
 
-    <section>
+
+    <section class="segundo">
         <h2>Precisão, estratégia e confiança definem a WYT. Com 98,2% de precisão em nossas análises, ajudamos empresas a tomarem decisões seguras e alcançarem resultados consistentes.</h2>
     </section>
 
-    <div>
+
+    <div class="terceiro">
         <img src="#" alt="">
     </div>
 
-    <section>
+
+    <section class="quarto">
         <p>Acreditamos que grandes negócios começam com planejamento. Por isso, criamos análises que reduzem riscos, identificam oportunidades e fortalecem projetos com estratégia e confiança.</p>
     </section>
 
+   
 
-    <footer >
+</div>
 
-     <div>
-      <h3>Suporte</h3>
-      <ul>
-        <li><a href="#">Central de Ajuda</a></li>
-        <li><a href="#">Política de Privacidade</a></li>
-        <li><a href="#">Termos de Uso</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
-    </div>
 
+<script>
+    const bussola = document.getElementById("bussola");
+    const menu = document.getElementById("menu");
+
+    bussola.addEventListener("click", () => {
+        bussola.classList.toggle("girada");
+        menu.classList.toggle("abrir");
+    });
+</script>
+
+</body>
+ <footer >
+      
     <div>
       <h3>Contato</h3>
       <p> Avenida Paulista, 1636 – Bela Vista, São Paulo – SP, 01310-200</p>
@@ -81,16 +113,8 @@ if (isset($_SESSION['empresa_id'])) {
       <p> wyt@gmail.com.br</p>
     </div>
 
- <div>
-      <h3>Social</h3>
-      <ul>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Tiktok</a></li>
-      </ul>
-    </div>
+
 
     <p>&copy; 2026 WYT - Todos os direitos reservados</p>
-</footer>
-</body>
+</footer> 
 </html>
